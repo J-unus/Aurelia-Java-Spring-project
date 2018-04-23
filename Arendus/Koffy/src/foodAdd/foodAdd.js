7500c87d03e20b75a3b28bdeee495aa7e91673f7
Marti Mölder <marti.molder@ttu.ee>
2018-04-23 13:29:34 +0300

    Parandused katekooria lisamisega

--- Arendus/Koffy/src/foodAdd/foodAdd.js
+++ Arendus/Koffy/src/foodAdd/foodAdd.js
@@ -34,7 +34,6 @@ export class foodAdd {
       .then(data => {
         client.fetch('http://localhost:8080/restaurants/1')
           .then(response => response.json())
-          .then(restaurants => this.restaurantList = restaurants)
           .then(data2 =>{
             console.log("category count= " + data2.foodCategories.length)
             deleteCategory(data2.foodCategories);
@@ -44,7 +43,6 @@ export class foodAdd {
     console.log("Category Add executed!")
 
     function deleteCategory(data){
-      console.log("deleteCategory")
     var id = data[data.length-1].id
     toAdd = "<div class='row' id='row"+ id + "'>" +
       "<div class='col-sm-12 col-xs-12 well well-sm text-center text-info item'>" +
@@ -72,13 +70,14 @@ export class foodAdd {
         'body': json(this.foodCategory)
       });
     $("#row"+ id).slideUp(100, function(){ $(this).remove();});
-    console.log("Delete executed!")
+    console.log("Category Delete executed!")
     });
     }
 
   }
 
   addFood() {
+    console.log("addFood")
     let client = new HttpClient();
     client.fetch('http://localhost:8080/foodCategories/addFood/1', {
         'method': "POST",
@@ -125,5 +124,40 @@ export class foodAdd {
 
   }
 
+  editCategory(id){
+    let client = new HttpClient();
+    if($("#btn"+id).val() == "edit"){
+      editCategory(id);
+    }else{
+      saveCategory(id);
+    }
+ 
+    function editCategory(id){
+      client.fetch('http://localhost:8080/foodCategories/' + id)
+        .then(response => response.json())
+        .then(data => {
+          $("#btn"+id).html('Save<span class="glyphicon glyphicon-check"></span>');
+          $("#btn"+id).toggleClass('btn-primary btn-success');
+          $("#btn"+id).attr("value", "save");
+          $("#editCategory"+id).html("<div class='form-group'><input type='text' class='form-control input-sm' id='input"+id+"' value='"+data.categoryName+"'></div>");
+        });
+    }
+
+    function saveCategory(id){
+      var foodCategory = $("#input"+id).val();
+      let foodCategorySave = { categoryName: foodCategory};
+      client.fetch('http://localhost:8080/foodCategories/'+ id, {
+        'method': "PUT",
+        'body': json(foodCategorySave)
+      });
+      console.log(id)
+      $("#btn"+id).html('Edit<span class="glyphicon glyphicon-edit"></span>');
+      $("#btn"+id).toggleClass('btn-success btn-primary');
+      $("#btn"+id).attr("value", "edit");
+      $("#editCategory"+id).html(foodCategory);
+      $("#"+id).hide().fadeIn(500);
+    }
+  }
+  
 
 }
